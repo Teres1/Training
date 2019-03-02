84bae94a0be6e5d1dc8b493700e0110c05d5788b
Slavi <slavi_14@abv.bg>
2019-03-02 21:45:01 +0200

    training

---
+++ CodingBat-functional2/src/NoLong.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoLong {
+    public List<String> noLong(List<String> strings) {
+        return strings.stream()
+                .filter(n->n.length()<4)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ CodingBat-functional2/src/NoZ.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoZ {
+    public List<String> noZ(List<String> strings) {
+        return strings.stream()
+                .filter(n->!n.contains("z")).collect(Collectors.toList());
+    }
+}
