f37d44d5884ab7366dbd3297c3b0fd228c332fe9
Slavi <slavi_14@abv.bg>
2019-05-05 17:57:56 +0300

    training

--- Judge-midFev/src/SymmetricArrays.java
+++ Judge-midFev/src/SymmetricArrays.java
@@ -6,17 +6,23 @@ public class SymmetricArrays {
     public static void main(String[] args) throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(rd.readLine());
-        int[]arr={1,2,3,2,1};
-        SymmetricArrays sa=new SymmetricArrays();
-        System.out.println(sa.isTelescopic(arr));
+        for (int i = 0; i < n; i++) {
+            String[] input = rd.readLine().trim().split(" ");
+            SymmetricArrays sa = new SymmetricArrays();
+            if(sa.isSymmetric(input)){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        }
     }
 
-    boolean isTelescopic(int[] a) {
-        if (a.length == 1) {
+    private boolean isSymmetric(String[]input) {
+        if (input.length == 1) {
             return true;
         } else {
-            for (int i = 0; i < (a.length + 1) / 2; i++) {
-                if (a[i]==a[a.length - 1 - i]) {
+            for (int i = 0; i < (input.length + 1) / 2; i++) {
+                if (input[i].equals(input[input.length - 1 - i])) {
                     return true;
                 } else {
                     return false;
